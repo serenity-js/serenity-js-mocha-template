4071fc4f533f6b3cab6f0e179d8e8f566e5ac2c3
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-01-29 11:25:38 +0000

    A place to start

---
+++ spec/mathjs-api/calculating_expressions.spec.ts
@@ -0,0 +1,48 @@
+import 'mocha';
+
+import { actorCalled } from '@serenity-js/core';
+import { Ensure, equals } from '@serenity-js/assertions';
+import { GetRequest, LastResponse, PostRequest, Send } from '@serenity-js/rest';
+import { escape } from 'querystring';
+
+describe('Math.js API', () => {
+
+    describe('GET /v4/?expr', () => {
+
+        // you can give domain meaning to your API requests by introducing
+        const RequestToCalculateExpression = (expression: string) =>
+            GetRequest.to(`/v4?expr=${ escape(expression) }`)
+                .describedAs(`a request to calculate ${ expression }`)
+
+        it('supports calculating a single expression', () =>
+            actorCalled('Apisitt').attemptsTo(
+                Send.a(RequestToCalculateExpression('2 + 2')),
+                Ensure.that(LastResponse.body<number>(), equals(4)),
+            ));
+    });
+
+    describe('POST /v4', () => {
+
+        const RequestToCalculateExpressions = (expressions: string[], precision?: number) =>
+            PostRequest.to(`/v4`).with({ expr: expressions, precision })
+                .describedAs(`a request to calculate ${ expressions.join(', ') }`)
+
+        interface CalculatedExpressions {
+            result: string[] | string | null;
+            error: string | null;
+        }
+
+        it('supports calculating multiple expressions in one request', () =>
+            actorCalled('Apisitt').attemptsTo(
+
+                Send.a(RequestToCalculateExpressions([ '2 + 2', '5 - 3' ])),
+
+                // note that LastResponse.body is generic;
+                // you can configure it with an interface specifying the shape of the expected response body
+                Ensure.that(LastResponse.body<CalculatedExpressions>(), equals({
+                    result: [ '4', '2' ],
+                    error: null,
+                })),
+            ));
+    });
+});
---
+++ spec/support/serenity.config.ts
@@ -0,0 +1,21 @@
+import 'mocha';
+
+import { ArtifactArchiver, configure } from '@serenity-js/core';
+import { SerenityBDDReporter } from '@serenity-js/serenity-bdd';
+import { ConsoleReporter } from '@serenity-js/console-reporter';
+import { Actors } from '../../src';
+
+/**
+ * @desc
+ *  Set up Serenity/JS reporting services before any scenarios are executed
+ *
+ * @see https://serenity-js.org/handbook/reporting/index.html
+ */
+configure({
+    actors: new Actors(process.env.BASE_API_URL || 'http://api.mathjs.org/v4/'),
+    crew: [
+        ConsoleReporter.withDefaultColourSupport(),
+        new SerenityBDDReporter(),
+        ArtifactArchiver.storingArtifactsAt(__dirname, '../../target/site/serenity'),
+    ]
+});
